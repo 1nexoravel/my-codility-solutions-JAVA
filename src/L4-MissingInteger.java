fbf334fe3f1d9c4349e4463818715dabb080e47e
1nexoravel <38869502+1nexoravel@users.noreply.github.com>
2020-05-27 23:36:59 -0300

    This

---
+++ src/L4-MissingInteger.java
@@ -0,0 +1,56 @@
+/*
+This is a demo task.
+
+        Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+        For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+        Given A = [1, 2, 3], the function should return 4.
+
+        Given A = [−1, −3], the function should return 1.
+
+        Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..100,000];
+        each element of array A is an integer within the range [−1,000,000..1,000,000].
+*/
+import java.util.Arrays;
+
+public class MissingInteger {
+    public static void main(String[] args) {
+        int A[] =  {1, 3, 6, 4, 1, 2};
+        System.out.println("A = [1, 3, 6, 4, 1, 2]");
+        System.out.println(Solution.solution(A));
+
+        int B[] = {1, 2, 3};
+        System.out.println("A = [1, 2, 3]");
+        System.out.println(Solution.solution(B));
+
+        int C[] = {-1, -3};
+        System.out.println("A = [−1, −3]");
+        System.out.println(Solution.solution(C));
+
+    }
+}
+class Solution {
+    public static int solution(int[] A) {
+        int b = 1;
+        Arrays.sort(A);
+        for (int k = 0 ; k < A.length ; k++) {
+            int c = A[k];
+            if( c > 0 ) {
+                if((b) == c) {
+                    b = c + 1;
+                }
+            }
+            else{
+                b = 1;
+            }
+        }
+        return b;
+    }
+}
